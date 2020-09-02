8bc0796dca30f85af48059346bed6f2949fc2824
diebaba <diebaba95@gmail.com>
2020-09-02 15:39:52 +0000

    proposition de contenue

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+//import { trigger, transition, animate, style } from '@angular/animations'
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
+ /*  animations:[
+    trigger('fade', [
+      transition('void => *', [style({ opacity: 0 }), animate('300ms', style({ opacity: 1 }))]),
+      transition('* => void', [style({ opacity: 1 }), animate('300ms', style({ opacity: 0 }))]),
+    ])
+  ] */
 })
 export class HeaderComponent implements OnInit {
 
   constructor() { }
+ ngOnInit() {
 
-  ngOnInit(): void {
-  }
+ }
+  
+  /* current = 0;
+  img_list = [
+    'https://picsum.photos/600/400/?image=0',
+    'https://picsum.photos/600/400/?image=1',
+    'https://picsum.photos/600/400/?image=2',
+  ];
+
+  ngOnInit() {
+    setInterval(() => {
+      this.current = ++this.current % this.img_list.length;
+    }, 2000);
+  } */
 
 }
